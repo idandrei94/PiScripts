9a616d3105f952dcf3c7c509f08d003f294e9755
daniorga <diorga@optimy.com>
2017-04-06 02:55:45 +0200

    basic vim config, jdk8, eclipse

---
+++ pi_init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get install -y vim build-essential cmake python-dev python3-dev openjdk-8-jdk eclipse eclipse-cdt git 
+echo "set exrc" >> /usr/share/vim/vimrc
+echo "set secure" >> /usr/share/vim/vimrc
+echo "set tabstop=4" >> /usr/share/vim/vimrc
+echo "set softtabstop=4" >> /usr/share/vim/vimrc
+echo "set shiftwidth=4" >> /usr/share/vim/vimrc
+echo "set noexpandtab" >> /usr/share/vim/vimrc
+echo "set colorcolumn=110" >> /usr/share/vim/vimrc
+echo "highlight ColorColumn ctermbg=darkgray" >> /usr/share/vim/vimrc
+echo "set number" >> /usr/share/vim/vimrc
