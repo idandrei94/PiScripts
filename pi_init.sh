f3e11fecb325a5e9182a6a45c96d791b9606746a
git <idandrei94@gmail.com>
2017-07-09 10:46:07 +0200

    apache -> apache2

--- pi_init.sh
+++ pi_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install -y vim build-essential cmake python-dev python3-dev openjdk-8-jdk eclipse eclipse-cdt apache mysql-server libmysqlcppconn-dev chromium-browser codeblocks
+apt-get install -y vim build-essential cmake python-dev python3-dev openjdk-8-jdk eclipse eclipse-cdt apache2 mysql-server libmysqlcppconn-dev chromium-browser codeblocks
 echo "set exrc" >> /usr/share/vim/vimrc
 echo "set secure" >> /usr/share/vim/vimrc
 echo "set tabstop=4" >> /usr/share/vim/vimrc
