1dc3aed507d271be4286d1cbb13303c3f5f3f7b6
Dan <idandrei94@gmail.com>
2019-04-20 18:11:24 +0200

    fixed log repetitive on/of"

--- relay/tempcontrol.sh
+++ relay/tempcontrol.sh
@@ -7,21 +7,35 @@ pumpoverride=$(cat /mnt/ramdisk/tempoverride)
 if [ ! -z "$pumpoverride" ] 
 then 
 	pump_on
-	echo $(date) Pump on >> /var/log/pump
+	if [ "$(cat /var/log/pump | tail -n 1 | awk '{print $8}')" != "on" ]; 
+	then 
+	    echo $(date) Pump on >> /var/log/pump 
+    fi
 	sleep 10
 else
+	if [ "$(cat /var/log/pump | tail -n 1 | awk '{print $8}')" != "off" ]; 
+	then 
+	    echo $(date) Pump off >> /var/log/pump 
+    fi
+fi
+
 if [ $temp -gt 42 ]
 then
 	pump_on	
-	echo $(date) Pump on >> /var/log/pump
+	if [ "$(cat /var/log/pump | tail -n 1 | awk '{print $8}')" != "on" ]; 
+	then 
+	    echo $(date) Pump on >> /var/log/pump 
+    fi
 	sleep 30
 fi
 
 if [ $temp -lt 30 ]
 then
 	pump_off
-	echo $(date) Pump off >> /var/log/pump
-fi
+	if [ "$(cat /var/log/pump | tail -n 1 | awk '{print $8}')" != "off" ]; 
+	then 
+	    echo $(date) Pump on >> /var/log/pump 
+    fi
 fi
 sleep 10
 done
