8cbaa45cfa0c7c1af6d2a7d061a19ccdf5ec63b8
Dan <idandrei94@gmail.com>
2019-04-20 17:29:18 +0200

    Added pump log

--- relay/tempcontrol.sh
+++ relay/tempcontrol.sh
@@ -7,17 +7,20 @@ pumpoverride=$(cat /mnt/ramdisk/tempoverride)
 if [ ! -z "$pumpoverride" ] 
 then 
 	pump_on
+	echo $(date) Pump on >> /car/log/pump
 	sleep 10
 else
 if [ $temp -gt 42 ]
 then
-	pump_on
+	pump_on	
+	echo $(date) Pump on >> /car/log/pump
 	sleep 30
 fi
 
 if [ $temp -lt 30 ]
 then
 	pump_off
+	echo $(date) Pump off >> /car/log/pump
 fi
 fi
 sleep 10
